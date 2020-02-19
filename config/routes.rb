c22ffd15dee79e8c5790a8690034bd38e92e57a6
ichigobranding <info@ichigo-branding.com>
2020-02-20 08:39:23 +0900

    モデルとマイグレーションファイルを同時に作成 データベースに反映 モデルのインスタンスを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
+  get 'posts/index' => "posts#index"
+  get '/' => "posts#index"
+  #get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
