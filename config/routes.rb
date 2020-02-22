e574b52e21e46b9b47ce1a9d2518b79d34bfbead
ichigobranding <info@ichigo-branding.com>
2020-02-22 09:35:50 +0900

    エラーテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index' => "posts#index"
-  get '/' => "posts#index"
+  # get 'posts/index' => "posts#index"
+  get "posts/index" => "posts#index"
+  get "/" => "home#top"
+  # get '/' => "posts#index"
   # get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
